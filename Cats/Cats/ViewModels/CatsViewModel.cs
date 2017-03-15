7c0422bab59b1e1c7b6321c67389e1668265e16f
Maiko <maikossmaster@gmail.com>
2017-03-14 22:43:08 -0300

    init

---
+++ Cats/Cats/ViewModels/CatsViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Cats.Models;
+using Xamarin.Forms;
+
+namespace Cats.ViewModels
+{
+    public class CatsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(
+            [System.Runtime.CompilerServices.CallerMemberName]
+            string propertyName = null) =>
+                PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(propertyName));
+
+        private bool Busy;
+
+        public bool IsBusy
+        {
+            get
+            {
+                return Busy;
+            }
+            set
+            {
+                Busy = value;
+                OnPropertyChanged();
+                GetCatsCommand.ChangeCanExecute();
+            }
+
+            
+        }
+
+
+        public ObservableCollection<Cat> Cats { get; set; }
+
+        public CatsViewModel()
+        {
+            Cats = new ObservableCollection<Models.Cat>();
+
+            GetCatsCommand = new Command(
+                async () => await GetCats(),
+                () => !IsBusy
+                );
+        }
+
+        async Task GetCats()
+        {
+
+            if (!IsBusy)
+            {
+                Exception Error = null;
+                try
+                {
+                    IsBusy = true;
+                    var repository = new Repository();
+                    var Items = await repository.GetCats();
+
+                    Cats.Clear();
+                    foreach (var Cat in Items)
+                    {
+                        Cats.Add(Cat);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Error = ex;
+                }
+                finally
+                {
+                    IsBusy = false;
+                    if (Error != null)
+                    {
+                        await Xamarin.Forms.Application.Current.MainPage.DisplayAlert(
+                            "Error!", Error.Message, "OK");
+                    }
+                }
+
+            }
+            return;
+        }
+
+        public Command GetCatsCommand { get; set; }
+
+
+
+    }
+}
